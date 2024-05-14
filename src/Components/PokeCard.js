42ff3707e4edb859b03b519af7f4d9f20e281429
Stephens\Raihanath <raihanalatheef775@gmail.com>
2024-05-14 15:57:01 +1200

    Added framer motion to animate on hover

--- src/Components/PokeCard.js
+++ src/Components/PokeCard.js
@@ -1,28 +1,50 @@
-import { FlexColumn } from './Styled/Flex.styled'
-import {StyledPokeCard, PokeCardInfo, PokeCardTypes}  from './Styled/PokeCard.styled'
-import { useNavigate } from 'react-router-dom'
+import { FlexColumn } from "./Styled/Flex.styled"
+import {
+  StyledPokeCard,
+  PokeCardInfo,
+  PokeCardTypes,
+} from "./Styled/PokeCard.styled"
+import { useNavigate } from "react-router-dom"
+import { motion } from "framer-motion"
+
+// Rotate figure on hover using motion
+const rotateMotion = {
+  rest: { rotate: 0, scale: 1 },
+  hover: {
+    rotateY: 180,
+    scale: 1.1,
+  },
+}
 
 export default function PokeCard(props) {
-    let pokeTypes = props.types
-    const pokeTypeForEach = pokeTypes.map(pokeType => <li key={pokeType.type.name} className={`type-${pokeType.type.name}`}>{pokeType.type.name}</li>)
-    let navigate = useNavigate()
-    return (
-        <StyledPokeCard>
-            <FlexColumn>
-                <PokeCardInfo onClick={() => {
-                    navigate(`/pokemon/${props.name}`, {state: props})
-                }} >
-                   <figure>
-                    <img src={props.sprites.other.dream_world.front_default} alt={props.name} />
-                   </figure>
-                    <span>#{String(props.id).padStart(3, '0')}</span>
-                    <h4>{props.name}</h4>
-                    <p>{props.url}</p>
-                    <PokeCardTypes>
-                        {pokeTypeForEach}
-                    </PokeCardTypes>
-                </PokeCardInfo>
-            </FlexColumn>
-        </StyledPokeCard>
-    )
+  let pokeTypes = props.types
+  const pokeTypeForEach = pokeTypes.map((pokeType) => (
+    <li key={pokeType.type.name} className={`type-${pokeType.type.name}`}>
+      {pokeType.type.name}
+    </li>
+  ))
+
+  let navigate = useNavigate()
+  return (
+    <StyledPokeCard initial="rest" whileHover="hover" animate="rest">
+      <FlexColumn>
+        <PokeCardInfo
+          onClick={() => {
+            navigate(`/pokemon/${props.name}`, { state: props })
+          }}
+        >
+          <motion.figure variants={rotateMotion}>
+            <img
+              src={props.sprites.other.dream_world.front_default}
+              alt={props.name}
+            />
+          </motion.figure>
+          <span>#{String(props.id).padStart(3, "0")}</span>
+          <h4>{props.name}</h4>
+          <p>{props.url}</p>
+          <PokeCardTypes>{pokeTypeForEach}</PokeCardTypes>
+        </PokeCardInfo>
+      </FlexColumn>
+    </StyledPokeCard>
+  )
 }
